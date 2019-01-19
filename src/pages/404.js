b0f190d68d11ce62604f57ebb7b5aad16999b0f3
Victor Tsang <vtsang@netbase.com>
2019-01-19 13:08:10 -0800

    404page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,16 @@
-import React from 'react'
-
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import React from 'react';
+import Content from '../components/Content/Content';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+	<Layout>
+		<SEO title="404: Not found" />
+		<Content noHero={true}>
+			<h1>NOT FOUND</h1>
+			<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+		</Content>
+	</Layout>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
